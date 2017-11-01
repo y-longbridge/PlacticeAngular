764b793c57b70c1e48f731ebd62fd461f6abed4f
y-longbridge <ynagahashi0@gmail.com>
2017-11-02 07:06:22 +0900

    upd

--- chap4-2/src/app/app.component.ts
+++ chap4-2/src/app/app.component.ts
@@ -6,19 +6,28 @@ import { Component } from '@angular/core';
   `
     <div>
       <h2>日付/時刻を整形する　- date -</h2>
+      <p>デフォルト: {{ currentDate | date }}</p>
+
       <p>あらかじめ用意されている書式</p>
       <table>
         <tr><th>書式</th><th>概要</th><th>結果(ja-JP)</th></tr>
-        <tr><td>medium</td><td>普通の日付/時刻 (yMMMdjms)</td><td>2017年3月17日 15:26:06</td></tr>
-        <tr><td>short</td><td>短い日付/時刻 (yMdjm)</td><td>2017/3/17 15:28</td></tr>
-        <tr><td>fullDate</td><td>完全な日付 (yMMMMMEEEEd)</td><td>2017年3月17日金曜日</td></tr>
-        <tr><td>longDate</td><td>長い日付(yMMMMd)</td><td>2017年3月17日</td></tr>
-        <tr><td>mediumDate</td><td>普通の日付(yMMMd)</td><td>2017年3月17日</td></tr>
-        <tr><td>shortDate</td><td>短い日付 (yMd)</td><td>2017/3/17</td></tr>
-        <tr><td>mediumTime</td><td>普通の時刻 (jms)</td><td>15:26:06</td></tr>
-        <tr><td>shortTime</td><td>短い時刻 (jm)</td><td>15:26</td></tr>
+        <tr><td>medium</td><td>普通の日付/時刻 (yMMMdjms)</td><td>{{ currentDate | date: 'medium' }}</td></tr>
+        <tr><td>short</td><td>短い日付/時刻 (yMdjm)</td><td>{{ currentDate | date: 'short' }}</td></tr>
+        <tr><td>fullDate</td><td>完全な日付 (yMMMMMEEEEd)</td><td>{{ currentDate | date: 'fullDate' }}</td></tr>
+        <tr><td>longDate</td><td>長い日付(yMMMMd)</td><td>{{ currentDate | date: 'longDate' }}</td></tr>
+        <tr><td>mediumDate</td><td>普通の日付(yMMMd)</td><td>{{ currentDate | date: 'mediumDate' }}</td></tr>
+        <tr><td>shortDate</td><td>短い日付 (yMd)</td><td>{{ currentDate | date: 'shortDate' }}</td></tr>
+        <tr><td>mediumTime</td><td>普通の時刻 (jms)</td><td>{{ currentDate | date: 'mediumTime' }}</td></tr>
+        <tr><td>shortTime</td><td>短い時刻 (jm)</td><td>{{ currentDate | date: 'shortTime' }}</td></tr>
       </table>
     </div>
+    <p>個別の書式を指定</p>
+    <table>
+      <tr><th>指定子</th><th>概要</th><th>結果(ja-JP)</th></tr>
+      <tr><td>G</td><td>紀元前/後(短い)</td><td>{{ currentDate | date: 'G'}}</td></tr>
+      <tr><td>GGG</td><td>紀元前/後(普通)</td><td>{{ currentDate | date: 'GGG'}}</td></tr>
+      <tr><td>GGGG</td><td>紀元前/後(長い)</td><td>{{ currentDate | date: 'GGGG'}}</td></tr>
+      </table>
   `,
   styles:
   [`
@@ -64,5 +73,5 @@ import { Component } from '@angular/core';
 // i18nSelect   文字列に応じて出力を切り替え
 // async        Observable・Promiseによる樋津凰稀処理の結果を取得 Chap7
 export class AppComponent  {
-
+ currentDate = new Date();
 }
